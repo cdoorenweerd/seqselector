249a73333fac27a7f77437805ec50302c8992f5d
Camiel Doorenweerd <cdoorenweerd@Camiels-MacBook-Pro.local>
2018-03-22 13:12:05 -1000

    takes argv now

--- seqselector.py
+++ seqselector.py
@@ -3,15 +3,20 @@
 # Camiel Doorenweerd 2018
 # Select a subset of sequences from a FASTA file using a csv file with a 
 # list of matching sample names. Uses Biopython. 
+# usage: python seqselector.py masterfasta.fas wishlist.csv
 
 import csv
+import sys
 from Bio import SeqIO
 
+masterfasta = sys.argv[1]
+wishlistcsv = sys.argv[2]
+
 selection = []
-with open("wishlist2.csv", "rb") as wishlistfile: 
+with open(wishlistcsv, "rb") as wishlistfile: 
     reader = csv.reader(wishlistfile)
     wishlist = list(reader)
-for seq_record in SeqIO.parse("COI_3_18_18.fasta", "fasta"):
+for seq_record in SeqIO.parse(masterfasta, "fasta"):
     if str(seq_record.id) in str(wishlist) :
         selection.append(seq_record)
-SeqIO.write(selection, "selection3.fas", "fasta")
+SeqIO.write(selection, "Selection_" + infile1, "fasta")
