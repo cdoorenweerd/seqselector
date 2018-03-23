9a2192014b87db9bce36b2069a62889276d4aa3b
Camiel Doorenweerd <cdoorenweerd@Camiels-MacBook-Pro.local>
2018-03-22 14:17:21 -1000

    argparse instead of sysarg

--- seqselector.py
+++ seqselector.py
@@ -3,14 +3,21 @@
 # Camiel Doorenweerd 2018
 # Select a subset of sequences from a FASTA file using a csv file with a 
 # list of matching sample names. Uses Biopython. 
-# usage: python seqselector.py masterfasta.fas wishlist.csv
 
 import csv
-import sys
+import argparse
 from Bio import SeqIO
+parser = argparse.ArgumentParser()
 
-masterfasta = sys.argv[1]
-wishlistcsv = sys.argv[2]
+#-i inputfile -w wishlist
+parser.add_argument("-i", "--fastainput",
+                    help="Inputfile in fasta format")
+parser.add_argument("-w", "--wishlist",
+                    help="CSV list with sequence names to select")
+
+args = parser.parse_args()
+masterfasta = args.fastainput
+wishlistcsv = args.wishlist
 
 selection = []
 with open(wishlistcsv, "rb") as wishlistfile: 
@@ -19,4 +26,4 @@ with open(wishlistcsv, "rb") as wishlistfile:
 for seq_record in SeqIO.parse(masterfasta, "fasta"):
     if str(seq_record.id) in str(wishlist) :
         selection.append(seq_record)
-SeqIO.write(selection, "Selection_" + infile1, "fasta")
+SeqIO.write(selection, "Selection_" + masterfasta, "fasta")
